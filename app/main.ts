d6c07ae02f92b3a3bec9885f247b14f0d6554ce6
Victor Emanuel Vlad <vev97@ymail.com>
2018-10-27 22:32:44 +0300

    basic integration done

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,22 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
-
 import { AppModule } from "./app.module";
 
+
+import firebase = require("nativescript-plugin-firebase");
+
+firebase.init({
+  //persist should be set to false as otherwise numbers aren't returned during livesync
+  persist: false,
+  storageBucket: 'gs://vev-project-lg.appspot.com'
+}).then(
+  function (instance) {
+    console.log("firebase.init done");
+  },
+  function (error) {
+    console.log("firebase.init error: " + error);
+  }
+  );
 // A traditional NativeScript application starts by initializing global objects, setting up global CSS rules, creating, and navigating to the main page.
 // Angular applications need to take care of their own initialization: modules, components, directives, routes, DI providers.
 // A NativeScript Angular app needs to make both paradigms work together, so we provide a wrapper platform object, platformNativeScriptDynamic,
